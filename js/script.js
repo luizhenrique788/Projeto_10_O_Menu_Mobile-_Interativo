57d77f486e3961c977c76fcc758bd3a2b8842cf6
Luiz Henrique <luizhenriquesouza866@icloud.com>
2025-11-30 01:31:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const mainNav = document.querySelector('.main-nav');
+mobileMenuIcon.addEventListener('click', () => {
+mainNav.classList.toggle('active');
+
+    });
+
+});
